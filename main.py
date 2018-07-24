8547d8264a4ff6e6c411670ca60f66ac5416579b
Demo User <demouser@MacBook-Pro.local>
2018-07-24 15:14:58 -0400

    I created all the questions in JavaScript and HTML. I linked them to main.py so they would display in the results page as well as there total. I updated them in the database.

--- main.py
+++ main.py
@@ -16,20 +16,32 @@ class HomeHandler(webapp2.RequestHandler):
             'templates/test.html')
         self.response.write(template2.render())
     def post(self):
-        answer=int(self.request.get("options"))
-        answer2=int(self.request.get("options2"))
-        user_answer=QuizAnswer(answer=answer, answer2=answer2)
-        user_answer.put()
+        count=1
+        answer_list=[]
+        total=0
+        #assigns all questions to a variable and puts then in a list
+        for i in range(8):
+            answer=int(self.request.get("q"+str(count)))
+            total+=answer;
+            answer_list.append(answer);
+            count+=1
+        #puts all questions into the database
+        for i in range(8):
+            index=answer_list[i]
+            user_answer=QuizAnswer(answer=index)
+            user_answer.put()
+
         template_vars={
-            "option": answer,
-            "option2": answer2,
+            "answer_list": answer_list,
+            "total":total, 
         }
         template = jinja_current_directory.get_template('templates/test_output.html')
         self.response.write(template.render(template_vars))
 
+
+
 class QuizAnswer(ndb.Model):
      answer=ndb.IntegerProperty()
-     answer2=ndb.IntegerProperty()
 
 
 app = webapp2.WSGIApplication([
