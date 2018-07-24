9f20dffe9d5bb8186645cce2c89eb982480ded80
Demo User <demouser@MacBook-Pro.local>
2018-07-24 08:18:52 -0400

    I created a class that stores the user input into a database.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import webapp2
 import os
 import random
 import jinja2
-
+from google.appengine.ext import ndb
 
 #remember, you can get this by searching for jinja2 google app engine
 jinja_current_directory = jinja2.Environment(
@@ -16,9 +16,9 @@ class HomeHandler(webapp2.RequestHandler):
             'templates/test.html')
         self.response.write(template2.render())
     def post(self):
-        answer=self.request.get("options")
-        answer2=self.request.get("options2")
-        user_answer=QuizAnswer(self.answer=answer,self.answer2=answer2)
+        answer=int(self.request.get("options"))
+        answer2=int(self.request.get("options2"))
+        user_answer=QuizAnswer(answer=answer, answer2=answer2)
         user_answer.put()
         template_vars={
             "option": answer,
@@ -28,8 +28,8 @@ class HomeHandler(webapp2.RequestHandler):
         self.response.write(template.render(template_vars))
 
 class QuizAnswer(ndb.Model):
-     answer=nbd.IntegerProperty()
-     answer2=nbd.IntegerProperty()
+     answer=ndb.IntegerProperty()
+     answer2=ndb.IntegerProperty()
 
 
 app = webapp2.WSGIApplication([
