1d262cb18d919b7c601e9977ed05c382b0781161
Demo User <demouser@MacBook-Pro.local>
2018-07-25 17:26:43 -0400

    I created another file to avoid merge errors.

--- main2.py
+++ main2.py
@@ -10,7 +10,7 @@ jinja_current_directory = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-class HomeHandler(webapp2.RequestHandler):
+class InsecuritiesHandler(webapp2.RequestHandler):
     def get(self):
         template2 = jinja_current_directory.get_template('templates/test.html')
         question_list=[
@@ -23,9 +23,11 @@ class HomeHandler(webapp2.RequestHandler):
         "I like to attend social events where I am involved in the festivities",
         "I feel as if I have a hard time making new friends",
         ]
-
+        title="Insecurities"
+        #Beginning
         template_vars={
         "question_list":question_list,
+        "title":title
         }
 
         self.response.write(template2.render(template_vars))
@@ -48,7 +50,7 @@ class HomeHandler(webapp2.RequestHandler):
             print index
             user_answer=QuizAnswer(answer=index, total=total)
             user_answer.put()
-
+        #End
         template_vars={
             "answer_list": answer_list,
             "total":total,
@@ -56,11 +58,56 @@ class HomeHandler(webapp2.RequestHandler):
         template = jinja_current_directory.get_template('templates/test_output.html')
         self.response.write(template.render(template_vars))
 
+class MinoritiesHandler(webapp2.RequestHandler):
+    def get(self):
+        template2 = jinja_current_directory.get_template('templates/test.html')
+        question_list1=[
+        "I feel like I can't relate to other people",
+        "My appearances always stand out amongst others",
+        "I am confident in myself",
+        "I feel like I am judged because of my appearance",
+        "I feel misunderstood",
+        "I think being unique is a good thing",
+        "I sometimes wish I looked differently",
+        "I am happy that I look differently than others",
+        ]
+        title="Minorities"
+        #Beginning
+        template_vars={
+        "question_list":question_list1,
+        "title":title
+        }
+        self.response.write(template2.render(template_vars))
+    def post(self):
+        count=1
+        answer_list=[]
+        total=0
+        #assigns all questions to a variable and puts then in a list
+        print(self.request)
+        for i in range(8):
+            answer=int(self.request.get("q"+str(count)))
+            total+=answer
+            answer_list.append(answer)
+            count+=1
+         #puts all questions into the database
+        for i in range(8):
+            index=answer_list[i]
+            print index
+            user_answer=QuizAnswer(answer=index, total=total)
+            user_answer.put()
+        #End
+        template_vars={
+            "answer_list": answer_list,
+            "total": total,
+        }
+        template = jinja_current_directory.get_template('templates/test_output.html')
+        self.response.write(template.render(template_vars))
 
 class QuizAnswer(ndb.Model):
     answer=ndb.IntegerProperty()
     total=ndb.IntegerProperty()
 
 app = webapp2.WSGIApplication([
-('/', HomeHandler),
+('/', InsecuritiesHandler),
+('/minorities', MinoritiesHandler)
 ], debug=True)
